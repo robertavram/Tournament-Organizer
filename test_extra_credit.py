74e347aab54d950684f4cb6490febf928243875e
Robert Avram <robert@ravdev.com>
2015-02-24 12:09:07 -0800

    small change, print info

--- test_extra_credit.py
+++ test_extra_credit.py
@@ -58,7 +58,7 @@ def testReportMatches(tournament="Default Tournament", tot_players=4):
     registerPlayers(tournament, tot_players)
     
     rounds = int(ceil(log(tot_players, 2)))
-    
+    print "PLAYING {} ROUNDS".format(rounds)
     for i in range(rounds):
         pairings = swissSupPairings(tournament)
         gameon(pairings, tournament)
